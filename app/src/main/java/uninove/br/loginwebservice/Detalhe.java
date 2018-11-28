122b07f4412190ec8a94686230125a9ea6a9a3b0
lpo19Lucas <po.lucas16@yahoo.com>
2018-11-28 20:28:14 -0200

    updating manifest

--- app/src/main/java/uninove/br/loginwebservice/Detalhe.java
+++ app/src/main/java/uninove/br/loginwebservice/Detalhe.java
@@ -36,6 +36,8 @@ public class Detalhe extends AppCompatActivity {
 
         String itemProcurado = params.getString("nome");
 
+        int userId = 0;
+
         for (Contato contato : contatos) {
             if (contato.getTitle().equals(itemProcurado)) {
                 titulo = (EditText) findViewById(R.id.titulo2);
@@ -46,6 +48,7 @@ public class Detalhe extends AppCompatActivity {
 
                 titulo.setText(contato.getTitle());
                 conteudo.setText(contato.getContent());
+                userId = contato.getId();
 //                email.setText(contato.getContent());
 //                criado.setText(contato.getCreatedAt());
 //                atualizado.setText(contato.getUpdatedAt());
@@ -58,17 +61,105 @@ public class Detalhe extends AppCompatActivity {
         salvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                titulo.setText("aaa");
+                checkNetworkConnection();
             }
         });
 
         excluir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                conteudo.setText("aaa");
+
             }
         });
 
+    }
+
+    public boolean checkNetworkConnection() {
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        boolean isConnected = false;
+        if (networkInfo != null && (isConnected = networkInfo.isConnected())) {
+            Toast.makeText(this, "Conectado" + networkInfo.getTypeName(), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Sem Acesso a Rede", Toast.LENGTH_SHORT).show();
+        }
+
+        return isConnected;
+    }
+
+
+    private String httpPost(String myUrl) throws IOException, JSONException {
+
+        URL url = new URL(myUrl);
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("PUT");
+        conn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+
+        JSONObject jsonObject = buidJsonObject();
+
+        setPostRequestContent(conn, jsonObject);
+
+        conn.connect();
+
+        Log.i("Retorno: ", conn.getResponseMessage().toUpperCase());
+        return conn.getResponseMessage().toUpperCase();
+
+    }
+
+    private class HTTPAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                try {
+                    return httpPost(urls[0]);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    return "Erro!";
+                }
+            } catch (IOException e) {
+                return "Sem acesso a página";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            tvResult.setText(result);
+            tvResult.setText("Retorno: " + result);
+        }
+    }
+
+
+    public void send(View view) {
+        // faz o HTTP POST request
+        if (checkNetworkConnection())
+            new HTTPAsyncTask().execute("https://serene-meadow-32620.herokuapp.com/api/notes");
+
+        else
+            Toast.makeText(this, "Sem Conexão!", Toast.LENGTH_SHORT).show();
+
+    }
+
+    private JSONObject buidJsonObject() throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.accumulate("id", userId.toString());
+        jsonObject.accumulate("title", titulo.getText().toString());
+        jsonObject.accumulate("content", conteudo.getText().toString());
+
+        return jsonObject;
+    }
+
+    private void setPostRequestContent(HttpURLConnection conn, JSONObject jsonObject) throws IOException {
+
+        OutputStream os = conn.getOutputStream();
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+        writer.write(jsonObject.toString());
+        Log.i(MainActivity.class.toString(), jsonObject.toString());
+        writer.flush();
+        writer.close();
+        os.close();
     }
 
     // Botão na ActionBar (Barra Superior)
